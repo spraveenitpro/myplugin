2faa22427940ef7d7f5fd32dc80e9301bcef55c1
spraveenitpro <praveen.selvasekaran@automattic.com>
2017-12-12 22:35:12 +0530

    Committing folder structure

---
+++ myplugin.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name:  MyPlugin
+Description:  Example plugin for "WordPress: Plugin Development"
+Plugin URI:   https://profiles.wordpress.org/spraveenitpro
+Author:       Praveen
+Version:      1.0
+Text Domain:  myplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+// exit the file if accessed directly
+
+if ( ! defined  ('ABSPATH')) {
+    exit;
+}
